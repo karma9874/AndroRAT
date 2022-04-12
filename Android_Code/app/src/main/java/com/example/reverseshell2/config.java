75c782f4a213d88fb80f0975e5453adb66656ecb
Muhammad Afiq <99951111+afiq2221@users.noreply.github.com>
2022-04-12 13:00:09 +0800

    Update config.java

--- Android_Code/app/src/main/java/com/example/reverseshell2/config.java
+++ Android_Code/app/src/main/java/com/example/reverseshell2/config.java
@@ -1,7 +1,7 @@
 package com.example.reverseshell2;
 
 public class config {
-    public static String IP = "192.168.0.105";
-    public static String port = "8888";
+    public static String IP = "192.168.100.50";
+    public static String port = "8777";
     public static boolean icon = true;
 }
